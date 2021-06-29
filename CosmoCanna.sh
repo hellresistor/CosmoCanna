8c0941cba2b140b05b683aaf0901b781885f232b
hellresistor <50468493+hellresistor@users.noreply.github.com>
2021-06-29 19:45:21 +0100

    v2.1

--- CosmoCanna.sh
+++ CosmoCanna.sh
@@ -3,7 +3,7 @@
 #-------------------------------------------#
 #   A BitCanna-Cosmos-Validator Lazy Tool   #
 #-------------------------------------------#
-#            Version: V2.00                 #
+#            Version: V2.10                 #
 #        Donate BitCanna Address:           #
 #--> B73RRFVtndfPRNSgSQg34yqz4e9eWyKRSv <-- #
 #-------------------------------------------#
@@ -21,19 +21,26 @@ fi
 }
 
 function getwalletinfo(){
+BCNACHAINID="bitcanna-testnet-4"
 MYMoniker=$(curl http://localhost:26657/status | grep -Po '"moniker": "\K.*?(?=")')
 MYVALIDADDRESS=$(curl -s http://localhost:26657/genesis | grep -A13 "$MYMoniker" | tail -n1 | grep -Po '"validator_address": "\K.*?(?=")')
 MYDELEGADDRESS=$(curl -s http://localhost:26657/genesis | grep -A12 "$MYMoniker" | tail -n1 | grep -Po '"delegator_address": "\K.*?(?=")')
 MYADDRESS=$MYDELEGADDRESS
 MYAvaliableBal=$(bcnad query bank balances "$MYDELEGADDRESS" --output json | jq | grep -Po '"amount": "\K.*?(?=")')
 MYCommiBalance=$(bcnad query distribution commission "$MYVALIDADDRESS" --output json | jq  | grep -Po '"amount": "\K.*?(?=")')
-MyRewardBalance=$(bcnad query distribution rewards "$MYDELEGADDRESS" --output json --chain-id bitcanna-testnet-3 | jq | grep -A4 "total" |  grep -Po '"amount": "\K.*?(?=")')
+MyRewardBalance=$(bcnad query distribution rewards "$MYDELEGADDRESS" --output json --chain-id "$BCNACHAINID" | jq | grep -A4 "total" |  grep -Po '"amount": "\K.*?(?=")')
 }
 
-function setsourceaddress(){
-info "Put Source Validator/Operator/Wallet address:"
-read -r THESADDRESS
-THESADDRESS=${THESADDRESS:-$MYADDRESS}
+function setsourcewaddress(){
+info "Put Source Wallet address:"
+read -r THESWADDRESS
+THESWADDRESS=${THESWADDRESS:-$MYADDRESS}
+}
+
+function setdestwaddress(){
+info "Put Target Wallet address:"
+read -r THEDWADDRESS
+THEDWADDRESS=${THEDWADDRESS:-$MYADDRESS}
 }
 
 function setamount(){
@@ -42,17 +49,24 @@ read -r THEAMOUNT
 THEAMOUNT=${THEAMOUNT:-$MYAMOUNT}
 }
 
-function setdestaddress(){
+function setsourceoaddress(){
 info "Put Target Validator/Operator address:"
-read -r THEDADDRESS
-THEDADDRESS=${THEDADDRESS:-$MYADDRESS}
+read -r THESOADDRESS
+THESOADDRESS=${THESOADDRESS:-$MYVALIDADDRESS}
+}
+
+function setdestoaddress(){
+info "Put Target Validator/Operator address:"
+read -r THEDOADDRESS
+THEDOADDRESS=${THEDOADDRESS:-$MYVALIDADDRESS}
 }
 
 function menu(){
-ok "Welcome To CosmoCanna Lazy-Tool"
+ok "Welcome To CosmoCanna Lazy-Tool by hellresistor"
 sleep 2
 while true
 do
+getwalletinfo
 info "
 My Moniker: $MYMoniker
 My Validator Address: $MYVALIDADDRESS
@@ -76,25 +90,25 @@ Q- Bye Bye
 Choice:"
 read -r choicy
 case $choicy in
- 1) bcnad tx distribution withdraw-all-rewards --from "$MYMoniker" "$GASFEE" --memo "Withdraw All rewards by CosmoCanna-Lazy tool" --chain-id "$BCNACHAIN" ;;
- 2) setdestaddress
+ 1) bcnad tx distribution withdraw-all-rewards --from "$MYMoniker" "$GASFEE" --memo "Withdraw All rewards by CosmoCanna-Lazy tool" --chain-id "$BCNACHAINID" ;;
+ 2) setdestoaddress
     setamount
-    bcnad tx staking delegate "$THEDADDRESS" "$THEAMOUNT"ubcna --from "$MYMoniker" "$GASFEE" --memo "Delegate by CosmoCanna-Lazy tool" --chain-id "$BCNACHAIN" ;;
- 3) setsourceaddress
-    setdestaddress
+    bcnad tx staking delegate "$THEDOADDRESS" "$THEAMOUNT"ubcna --from "$MYMoniker" "$GASFEE" --memo "Delegate by CosmoCanna-Lazy tool" --chain-id "$BCNACHAINID" ;;
+ 3) setsourceoaddress
+    setdestoaddress
     setamount
-    bcnad tx staking redelegate "$THESADDRESS" "$THEDADDRESS" "$THEAMOUNT"ubcna --from "$MYMoniker" "$GASFEE" --memo "Redelegate by CosmoCanna-Lazy tool" --chain-id "$BCNACHAIN" ;;
- 4) setsourceaddress
-    setdestaddress
+    bcnad tx staking redelegate "$THESOADDRESS" "$THEDOADDRESS" "$THEAMOUNT"ubcna --from "$MYMoniker" "$GASFEE" --memo "Redelegate by CosmoCanna-Lazy tool" --chain-id "$BCNACHAINID" ;;
+ 4) setsourcewaddress
+    setdestwaddress
     setamount
-    bcnad tx bank send "$THESADDRESS" "$THEDADDRESS" "$THEAMOUNT"ubcna -y "$GASFEE" --memo "Send Bcna by CosmoCanna-Lazy tool" --chain-id "$BCNACHAIN" ;;
+    bcnad tx bank send "$THESWADDRESS" "$THEDWADDRESS" "$THEAMOUNT"ubcna -y "$GASFEE" --memo "Send Bcna by CosmoCanna-Lazy tool" --chain-id "$BCNACHAINID" ;;
  5)  ;;
- 6) setdestaddress
+ 6) setdestwaddress
     setamount
-    bcnad tx staking unbond "$THEDADDRESS" "$THEAMOUNT"ubcna --from "$MYMoniker" "$GASFEE" --memo "Unbond by CosmoCanna-Lazy tool" --chain-id "$BCNACHAIN" ;;
- 7) bcnad tx slashing unjail --from "$MYMoniker" "$GASFEE" --memo "Unjailing by CosmoCanna-Lazy tool" --chain-id "$BCNACHAIN" ;;
+    bcnad tx staking unbond "$THEDWADDRESS" "$THEAMOUNT"ubcna --from "$MYMoniker" "$GASFEE" --memo "Unbond by CosmoCanna-Lazy tool" --chain-id "$BCNACHAINID" ;;
+ 7) bcnad tx slashing unjail --from "$MYMoniker" "$GASFEE" --memo "Unjailing by CosmoCanna-Lazy tool" --chain-id "$BCNACHAINID" ;;
  q|Q) ok "Bye Bye Roll One joint for me ;)" && exit 0 ;;
- *) warn "MISSING KEY"
+ *) warn "MISSING KEY" && sleep 0.5;;
 esac
 done
 }
@@ -103,5 +117,4 @@ done
 ###  Start  ###
 ###############
 checkservicestatus
-getwalletinfo
 menu
