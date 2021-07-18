2335862b22cc6cc1b6aeef591947a5ad5d6bca30
hellresistor <50468493+hellresistor@users.noreply.github.com>
2021-07-18 08:52:14 +0100

    Update CosmoCanna-IBC.sh

--- CosmoCanna-IBC.sh
+++ CosmoCanna-IBC.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC1091,SC2154
+# shellcheck disable=SC1091,SC2154 source=~/.CONFIG
 #---------------------------------------------#
 #             IBC Transfer Tool               #
 #---------------------------------------------#
