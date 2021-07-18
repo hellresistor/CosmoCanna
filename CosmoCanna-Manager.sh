cf9fe747028b02f738ed99e599347c8491b15552
hellresistor <50468493+hellresistor@users.noreply.github.com>
2021-07-18 08:57:14 +0100

    Update CosmoCanna-Manager.sh

--- CosmoCanna-Manager.sh
+++ CosmoCanna-Manager.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC1091
+# shellcheck disable=SC1091,SC2154
 #-------------------------------------------#
 #   A BitCanna-Cosmos-Validator Lazy Tool   #
 #-------------------------------------------#
